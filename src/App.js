5d25cc89e8fae2ba6c3d7d45378dc1eb4e208d49
flaskur <flaskur@gmail.com>
2020-03-11 14:43:15 -0400

    hats and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+
+const HatPage = function({ history, location, match }) {
+	console.log(history);
+	console.log(location);
+	console.log(match);
+
+	return (
+		<div>
+			<h1>hatspage</h1>
+			<h2>{match.params.someNumber}</h2>
+		</div>
+	);
+};
 
 const App = function() {
 	return (
 		<div className="App">
 			<h1>APPTAKEN</h1>
-			<Homepage />
+			{/* <Homepage /> */}
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route exact path="/hats" component={HatPage} />
+				<Route path="/hats/:someNumber" component={HatPage} />
+			</Switch>
 		</div>
 	);
 };
@@ -16,4 +35,8 @@ export default App;
 /**
  * Design planning, assume that the App component is your baseline. Everything extends from that and the index.css is just default formatting like making margins 0.
  * Grid menu should use CSS grid with 1/3 and 1/2 formatting, but should have a media query of spacing to be 1 eventually.
+ * 
+ * Use the exact keyword if you want the exact path. Not sure why, but you should wrap the routes in a Switch tag. If you use these routes and specify a component, there will be 3 props attached to that component. Namely history, location, match. History has the push function, location and match have the pathname. This is useful because you can find the current url using this extrapolation instead of knowing what path you are currently on. You would not use the exact keyword if you had a parameter path using :. You can access the params given using props.match.params.paramName. How would you make restricted paths?
+ * 
+ * Prop tunneling is relying way too much on the prop tree and making it confusing. This happens for the history stuff. Why do I need to keep track of history anyways? The withRouter property function allows us to avoid using prop drilling and instead give access to the history, location, and match by nesting the export component.
  */
--- src/components/menu_item/MenuItem.js
+++ src/components/menu_item/MenuItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; // Higher order component. Function that takes a component, modifies, and returns new component.
 import './MenuItem.scss';
 
-const MenuItem = function({ title, imageUrl, size }) {
+const MenuItem = function({ title, imageUrl, size, linkUrl, history, match }) {
 	return (
 		<div
 			// Adding interpolated size prop.
 			className={`${size} menu_item`}
+			// This syntax is correct because match.url has / at the end.
+			onClick={() => history.push(`${match.url}${linkUrl}`)}
 		>
 			{/* The background image styling is nested for CSS reasons. */}
 			<div
@@ -24,4 +27,4 @@ const MenuItem = function({ title, imageUrl, size }) {
 
 // My theory for styling would just to use the inline JS styling with ternary size checking if undefined. size === large ?
 
-export default MenuItem;
+export default withRouter(MenuItem); // To give access to history, location, match.
